154ac0f397ea90a77a87d88eb93989da9708aeab
Stefan Lovecchio <stefanlovecchio@gmail.com>
2023-05-25 20:14:57 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+    function Start()
+    {
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
